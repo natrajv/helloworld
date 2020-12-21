d26365ada09662ca874d89ed60905012bf58e506
natrajv <natrajv@yahoo.com>
2020-12-21 21:23:45 +0530

    HelloWorld MVC Razor

---
+++ helloWorld/Controllers/HelloController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace helloWorld.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        public ActionResult Index()
+        {
+            //Razor view
+            return View();
+        }
+    }
+}
